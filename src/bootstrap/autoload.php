ee34f756c4d247d8ec5a68d0bac11c60de90ab58
lunnlew <lunnlew@qq.com>
2015-12-18 02:11:39 +0800

    u

--- src/bootstrap/autoload.php
+++ src/bootstrap/autoload.php
@@ -25,16 +25,15 @@ defined('DEBUGLEVEL') or define('DEBUGLEVEL', 1);
 | into the script here so that we do not have to worry about the
 | loading of any our classes "manually". Feels great to relax.
 |
-*/
-require __DIR__.'/../../vendor/autoload.php';
+ */
+require __DIR__ . '/../../vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
 | Register The Composer Auto Loader
 |--------------------------------------------------------------------
 |
-*/
-require FRAME_PATH.'AutoloaderClassPsr4.php';
-
+ */
+require FRAME_PATH . 'AutoloaderClassPsr4.php';
 
 // instantiate the loader
 $loader = new \Norma\AutoloaderClassPsr4;
@@ -43,29 +42,28 @@ $loader = new \Norma\AutoloaderClassPsr4;
 $loader->register();
 
 $loader->addNamespace('Norma', FRAME_PATH);
-$loader->addNamespace('Norma', __DIR__.'/../../tests');
-
+$loader->addNamespace('Norma', __DIR__ . '/../../tests');
 
 //日志处理
-require('php_error.php');
+require 'php_error.php';
 \php_error\reportErrors();
-//环境完备性检测
-require FRAME_PATH.'bootstrap/Integrity.php';
-//框架基本参数设置
-require FRAME_PATH.'bootstrap/FrameParams.php';
 
+//环境完备性检测
+require FRAME_PATH . 'bootstrap/Integrity.php';
+//框架基本参数设置
+require FRAME_PATH . 'bootstrap/FrameParams.php';
 //加载框架核心
-if (RUN_MODE==='WEB') {
-    require FRAME_PATH . 'NormaCore.php';//WEB核心
+if (RUN_MODE === 'WEB') {
+    require FRAME_PATH . 'NormaCore.php'; //WEB核心
 } else {
-    require FRAME_PATH . 'NormaTask.php';//CLI核心
+    require FRAME_PATH . 'NormaTask.php'; //CLI核心
 }
 //加载引擎资源
-require_once(FRAME_PATH . 'Core/Engine/' . RUN_ENGINE . '.php');
+require_once FRAME_PATH . 'Core/Engine/' . RUN_ENGINE . '.php';
 
 //加载应用核心
 if (file_exists(APP_PATH . 'Custom/Norma.php')) {
     require APP_PATH . 'Custom/Norma.php';
 } else {
-    require FRAME_PATH . 'Norma.php';//加载空核心
+    require FRAME_PATH . 'Norma.php'; //加载空核心
 }
