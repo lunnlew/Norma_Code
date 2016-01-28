64d0fd717bbc4b338fbed432f63e2b74249da537
lunnlew <lunnlew@qq.com>
2016-01-28 15:41:23 +0800

    add UnknownServiceException

---
+++ src/Norma/Exception/UnknownServiceException.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | Norma
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015  All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author:  LunnLew <lunnlew@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace Norma\Exception;
+
+/**
+ * UnknownServiceException
+ *
+ * 当一个不存在的服务被调用时抛出的异常
+ *
+ * @uses       Exception
+ * @package    Klein\Exceptions
+ */
+class UnknownServiceException extends RuntimeException implements NormaExceptionInterface
+{
+}
