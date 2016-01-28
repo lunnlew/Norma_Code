05d97728212238596f36f9f9a4a7f9c9c90a8252
lunnlew <lunnlew@qq.com>
2016-01-28 15:51:09 +0800

    add BuildingException

---
+++ src/Norma/Exception/BuildingException.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | Norma
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015  All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author:  LunnLew <lunnlew@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace Norma\Exception;
+
+/**
+ * BuildingException
+ *
+ * 当Build出错时抛出的异常
+ */
+class BuildingException extends RuntimeException implements NormaExceptionInterface
+{
+}
